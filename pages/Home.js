6acf4d770c80e7fd89f856bc3e190a6fd93827a8
ayush-prajapat <ak3389510@gmail.com>
2024-11-04 15:56:49 +0530

    Add files via upload

---
+++ pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home(){
+return(
+<div>
+    <h2>Welcome to the home page </h2>
+    <p>This protected page accessable agter login </p>
+
+</div>
+
+ );
+
+}
+
+export default Home;
